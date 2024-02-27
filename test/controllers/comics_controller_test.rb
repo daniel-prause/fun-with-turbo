419e273edeaaae26f1d01bcd26852acbd681c9b8
Daniel Prause <daniel.prause@re-digital.io>
2024-02-27 14:13:58 +0100

    reformat comics controller

--- test/controllers/comics_controller_test.rb
+++ test/controllers/comics_controller_test.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ComicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get comics_url
     assert_response :success
   end
 
-  test "should create a new comic" do
+  test 'should create a new comic' do
     assert_difference('Comic.count', 1) do
       post comics_url, params: { comic: { name: 'New Comic', author_id: authors(:one).id } }
     end
     assert_response :success
   end
 
-  test "should not create a new comic with invalid params" do
+  test 'should not create a new comic with invalid params' do
     assert_no_difference('Comic.count') do
       post comics_url, params: { comic: { name: nil, author_id: authors(:one).id } }
     end
     assert_response :unprocessable_entity
   end
 
-  test "should destroy the specified comic" do
+  test 'should destroy the specified comic' do
     comic = comics(:one)
     assert_difference('Comic.count', -1) do
       delete comic_url(comic)
